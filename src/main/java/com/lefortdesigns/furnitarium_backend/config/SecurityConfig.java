0b35b3c3f7c63fe0908013147d2a4786c48117a2
zollidan <122796590+zollidan@users.noreply.github.com>
2023-12-13 16:46:44 +0300

    created not workin login via jwt

--- src/main/java/com/lefortdesigns/furnitarium_backend/config/SecurityConfig.java
+++ src/main/java/com/lefortdesigns/furnitarium_backend/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
         }
 
         // @Bean
+        // @Override
         // public AuthenticationManager authenticationManagerBean() throws Exception {
         // return super.authenticationManagerBean();
         // }
--- src/main/java/com/lefortdesigns/furnitarium_backend/conrollers/AuthController.java
+++ src/main/java/com/lefortdesigns/furnitarium_backend/conrollers/AuthController.java
@@ -3,15 +3,17 @@ package com.lefortdesigns.furnitarium_backend.conrollers;
 import java.util.Map;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lefortdesigns.furnitarium_backend.dto.AuthenticationDto;
 import com.lefortdesigns.furnitarium_backend.dto.PersonDto;
 import com.lefortdesigns.furnitarium_backend.models.Person;
 import com.lefortdesigns.furnitarium_backend.security.JwtUtil;
@@ -28,24 +30,20 @@ public class AuthController {
     private final RegistrationService registrationService;
     private final JwtUtil jwtUtil;
     private final ModelMapper modelMapper;
+    private final AuthenticationManager authenticationManager;
 
-    @Autowired
     public AuthController(PersonValidator personValidator, RegistrationService registrationService, JwtUtil jwtUtil,
-            ModelMapper modelMapper) {
+            ModelMapper modelMapper, AuthenticationManager authenticationManager) {
         this.personValidator = personValidator;
         this.registrationService = registrationService;
         this.jwtUtil = jwtUtil;
         this.modelMapper = modelMapper;
+        this.authenticationManager = authenticationManager;
     }
 
     @GetMapping("/login")
     public String loginPage() {
         return "login";
-    }
-
-    @GetMapping("/registration")
-    public String registrationPage(@ModelAttribute("person") Person person) {
-        return "registrationPage";
     }
 
     @PostMapping("/registration")
@@ -67,13 +65,29 @@ public class AuthController {
         return Map.of("jwt-token", token);
     }
 
-    // @PostMapping("/login")
-    // public Map<String, String> performingLogin(@RequestBody AuthenticationDto
-    // authenticationDto){
-    // UsernamePasswordAuthenticationToken authenticationToken = new
-    // UsernamePasswordAuthenticationToken(authenticationDto,
-    // authenticationDto.getEmail());
-    // }
+    // -----------------------
+    // -----------------------
+    // -----------------------
+    // -----------------------
+    // -----------------------
+    // -----------------------
+    // -----------------------
+    // он авторизует всех на похуй надо поправить
+    @PostMapping("/login")
+    public Map<String, String> performingLogin(@RequestBody AuthenticationDto authenticationDto) {
+        UsernamePasswordAuthenticationToken authenticationInputToken = new UsernamePasswordAuthenticationToken(
+                authenticationDto.getEmail(),
+                authenticationDto.getPassword());
+
+        try {
+            authenticationManager.authenticate(authenticationInputToken);
+        } catch (BadCredentialsException exception) {
+            Map.of("message", "bad credential");
+        }
+
+        String token = jwtUtil.generateToken(authenticationDto.getEmail());
+        return Map.of("jwt-token", token);
+    }
 
     public Person covertToPerson(PersonDto personDto) {
         return this.modelMapper.map(personDto, Person.class);
